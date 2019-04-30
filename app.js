9a4ca02e18c244fef4fa3bae3e4f9a74bda29ea3
rishabh_jitani <rishabh.j@simformsolutions.com>
2019-04-30 11:46:45 +0530

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
